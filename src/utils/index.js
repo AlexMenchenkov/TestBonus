026b8159f304c3dd3dbacb235273529a9aad4143
amenchenkov <16Elf@mail.ru>
2021-04-26 21:30:55 +0300

    MOD: fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,20 +13,21 @@ export const getRequestBody = () => {
 }
 
 export const getStringDeclination = (countOrigin, forms) => {
-	const count = Math.abs(countOrigin) % 100;// eslint-disable-line no-magic-numbers
-	const n1 = count % 10;// eslint-disable-line no-magic-numbers
-	if (count > 10 && count < 20) { return forms[2]; }// eslint-disable-line no-magic-numbers
-	if (n1 > ONE && n1 < 5) { return forms[ONE]; }// eslint-disable-line no-magic-numbers
+	const count = Math.abs(countOrigin) % 100;
+	const n1 = count % 10;
+	if (count > 10 && count < 20) { return forms[2]; }
+	if (n1 > ONE && n1 < 5) { return forms[ONE]; }
 	if (n1 === ONE) { return forms[ZERO]; }
-	return `${countOrigin} ${forms[2]}`;// eslint-disable-line no-magic-numbers
+	return `${countOrigin} ${forms[2]}`;
 }
 
 export const getFormatedDate = (date) => {
 	if (!date) {
 		return '';
 	}
+	const twoLastElements = -2;
 	const instanceDate = new Date(date);
-	const month = (`${ZERO}` + (instanceDate.getUTCMonth() + ONE)).slice(-2);// eslint-disable-line no-magic-numbers
+	const month = (`${ZERO}` + (instanceDate.getUTCMonth() + ONE)).slice(twoLastElements);
 	const day = instanceDate.getUTCDate();
 	return `${day}.${month}`
 }
